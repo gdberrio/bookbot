543b9901abece4bc7df360012af6adae558f3f5b
gdberrio <guilherme.diazberrio@gmail.com>
2025-03-23 17:38:44 +0000

    refactor

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+from stats import count_words
+
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
 
     return file_contents
-
-
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 
 def main():
