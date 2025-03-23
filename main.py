1d3cc89b78648025cff1abe391122ad13093f3b8
gdberrio <guilherme.diazberrio@gmail.com>
2025-03-23 17:36:48 +0000

    get_book_text and count_words

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def main():
+    frankenstein = get_book_text("./books/frankenstein.txt")
+    number_of_words = count_words(frankenstein)
+    print(f"{number_of_words} words found in the document")
+
+
+main()
